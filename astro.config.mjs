73ad8fb8fcb86847b5730bbf062b03fd8bc57aaa
Andreas Wessing <wessing.andreas@gmail.com>
2026-03-14 21:17:45 +0100

    define astro domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+//define domain
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://lennet.ac";
+
+});
